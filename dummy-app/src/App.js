4795f52ace6440605e2d4a2653bc3b32b47bad53
TuceYBLN <tuce_yemisen@hotmail.de>
2025-05-01 12:46:39 +0200

    React frontend angepasst

--- dummy-app/src/App.js
+++ dummy-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [name, setName] = useState("")
+
+    function onChangeHandler(e) {
+        setName(e.target.value);
+    }
+
+    return (
+        <div>
+            <label>Enter your name:
+                <input type="text" onChange={e => onChangeHandler(e)}/>
+            </label>
+            Dein Eingabe lautet: {name}
+        </div>
+    );
 }
 
 export default App;
