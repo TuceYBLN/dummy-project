53eb8b3487fb31491fe0beb497dbad28c3bf07c3
TuceYBLN <tuce_yemisen@hotmail.de>
2025-05-01 12:55:04 +0200

    AS-1 Beschriftung geändert

--- dummy-app/src/App.js
+++ dummy-app/src/App.js
@@ -14,7 +14,7 @@ function App() {
             <label>Enter your name:
                 <input type="text" onChange={e => onChangeHandler(e)}/>
             </label>
-            Dein Eingabe lautet: {name}
+            Deine Eingabe lautet: {name}
         </div>
     );
 }
