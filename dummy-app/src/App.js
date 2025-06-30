7f0e05d61d6ad3a1df4b1a1e9bc23a9e847662e2
TuceYBLN <tuce_yemisen@hotmail.de>
2025-06-30 22:27:08 +0200

    SCRUM-5:  something again

--- dummy-app/src/App.js
+++ dummy-app/src/App.js
@@ -11,7 +11,7 @@ function App() {
 
     return (
         <div>
-            <h1>willkommen</h1>
+            <h1>willkommen!</h1>
             <label>Enter your name:
                 <input type="text" onChange={e => onChangeHandler(e)}/>
             </label>
