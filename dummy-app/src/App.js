ed6930df828fc631cd91d888bbe1b04edb36bee9
TuceYBLN <tuce_yemisen@hotmail.de>
2025-07-05 13:35:54 +0200

    SCRUM-9: Zeilenumbruch hinzugefügt

--- dummy-app/src/App.js
+++ dummy-app/src/App.js
@@ -15,6 +15,7 @@ function App() {
             <label>Enter your name:
                 <input type="text" onChange={e => onChangeHandler(e)}/>
             </label>
+            <br/>
             Dein Eingabe lautet: {name}
         </div>
     );
